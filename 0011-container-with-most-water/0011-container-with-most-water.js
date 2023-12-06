24f5bcdbc3563ebd7a6d9324687860ef8e58125a
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2023-12-06 16:02:07 +0900

    Time: 71 ms (32.94%), Space: 49.5 MB (61.76%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,18 +3,32 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let answer = 0
-    const reversed = [...height].reverse()
-    const set = new Set(height)
-    
-    set.forEach(s => {
-        const idx = height.findIndex(h => s <= h)
-        const rIdx = reversed.length - 1 - reversed.findIndex(r => s <= r)
-        if(idx !== rIdx) {
-            const m = (rIdx-idx) * s
-            if(answer < m) answer = m
+    let left = 0
+    let right = height.length-1
+    let maxWater = 0
+    while (left < right) {
+        const width = right - left
+        const h = Math.min(height[left], height[right])
+        maxWater = Math.max(width*h, maxWater)
+        if(height[left] < height[right]) {
+            left++
+        } else {
+            right--
         }
-    })
+    }
+    return maxWater
+    // let answer = 0
+    // const reversed = [...height].reverse()
+    // const set = new Set(height)
     
-    return answer
+    // set.forEach(s => {
+    //     const idx = height.findIndex(h => s <= h)
+    //     const rIdx = reversed.length - 1 - reversed.findIndex(r => s <= r)
+    //     if(idx !== rIdx) {
+    //         const m = (rIdx-idx) * s
+    //         if(answer < m) answer = m
+    //     }
+    // })
+    
+    // return answer
 };
