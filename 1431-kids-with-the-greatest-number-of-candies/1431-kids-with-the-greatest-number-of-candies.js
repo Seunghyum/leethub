8f6863dfad2ecb6cb7261d945bb0c192910c9a93
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2023-12-09 23:23:34 +0900

    Time: 52 ms (68.76%), Space: 42.1 MB (79.52%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -4,6 +4,6 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    let max = candies.reduce((a,b) => a > b ? a : b, 0)
-    return candies.map(c => c + extraCandies >= max ? true : false)
+    const max = Math.max(...candies);
+    return candies.map(c => c + extraCandies >= max)
 };
