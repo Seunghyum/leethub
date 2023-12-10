80ff1413623f2994eff3a9ba45bbc3e1419e7297
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2023-12-11 01:34:31 +0900

    Time: 166 ms (43.61%), Space: 49.8 MB (58.30%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+var closeStrings = function(word1, word2) {
+    if(word1.length !== word2.length) return false
+    const map1 = {}
+    const map2 = {}
+    for(let i=0;i<word1.length;i++) {
+        if(map1[word1[i]] === undefined) map1[word1[i]] = 1
+        else map1[word1[i]]++
+
+        if(map2[word2[i]] === undefined) map2[word2[i]] = 1
+        else map2[word2[i]]++
+    }
+
+    for(const key in map1) {
+        if(map2[key] === undefined) return false
+    }
+    const values1 = Object.values(map1).sort()
+    const values2 = Object.values(map2).sort()
+    if(values1.length !== values2.length) return false
+    for(let i=0;i<values1.length;i++) {
+        if(values1[i] !== values2[i]) return false
+    }
+    return true
+};
