48db8d39cd3b37b175c32af4741009c2f28c2adc
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2023-12-15 17:54:07 +0900

    Time: 119 ms (87.48%), Space: 73.8 MB (65.48%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
@@ -12,7 +12,7 @@
  */
 var maxLevelSum = function(root) {
     let tree = []
-    bfs(root, 0)
+    dfs(root, 0)
     
     let max = - Math.pow(10,5)
     let maxIndex = 0
@@ -25,11 +25,66 @@ var maxLevelSum = function(root) {
     }
 
     return maxIndex + 1
-    function bfs(node, depth) {
+    function dfs(node, depth) {
         if(node === null) return
         if(tree[depth] === undefined) tree[depth] = node.val
         else tree[depth] += node.val
-        bfs(node.left, depth+1)
-        bfs(node.right, depth+1)
+        dfs(node.left, depth+1)
+        dfs(node.right, depth+1)
     }
 };
+/**
+- 내가 푼 답안 - dfs
+var maxLevelSum = function(root) {
+    let tree = []
+    dfs(root, 0)
+    
+    let max = - Math.pow(10,5)
+    let maxIndex = 0
+
+    for(let i=0;i<tree.length;i++) {
+        if(max<tree[i]) {
+            max = tree[i]
+            maxIndex = i
+        }
+    }
+
+    return maxIndex + 1
+    function dfs(node, depth) {
+        if(node === null) return
+        if(tree[depth] === undefined) tree[depth] = node.val
+        else tree[depth] += node.val
+        dfs(node.left, depth+1)
+        dfs(node.right, depth+1)
+    }
+};
+
+- 더 좋은 답안 bfs
+
+var maxLevelSum = function (root) {
+    let ans = 1;
+    if (!root) return ans;
+
+    let qu = [root];
+    let maxSum = root.val;
+    let height = 1;
+    while (qu.length) {
+        const n = qu.length;
+        let sum = 0;
+        for (let i = 0; i < n; i++) {
+            const node = qu.shift();
+            sum += node.val;
+            if (node.left) qu.push(node.left);
+            if (node.right) qu.push(node.right);
+        }
+        if(sum > maxSum){
+            maxSum = sum;
+            ans = height;
+        }
+
+        height++;
+    }
+    return ans;
+};
+
+ */
