db06c6718a6531b3a0e1e10787db5c936c727c5c
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2023-12-17 02:41:29 +0900

    Time: 148 ms (40.24%), Space: 92.5 MB (34.82%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.js
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.js
@@ -12,20 +12,68 @@
  */
 var longestZigZag = function(root) {
     let answer = 0
-    zigZagPathCount(root, 0, false, 1)
-    zigZagPathCount(root, 0, true, 1)
+    zigZagPathCount(root, 0, false)
+    zigZagPathCount(root, 0, true)
     return answer
     
-    function zigZagPathCount(node, num, isLeft, depth) {
+    function zigZagPathCount(node, num, isLeft) {
         if(!node) return
 
         answer = Math.max(answer,num)
-        if(isLeft) {
-            zigZagPathCount(node.left, num+1, !isLeft, depth)
-            zigZagPathCount(node.right,  1, isLeft, depth)
-        } else {
-            zigZagPathCount(node.left, 1, isLeft, depth)
-            zigZagPathCount(node.right, num+1, !isLeft, depth)
-        }
+        zigZagPathCount(node.left, isLeft === true ? num+1 : 1, false)
+        zigZagPathCount(node.right, isLeft === false ? num+1 : 1, true)
+
     }
 };
+
+/**
+    
+- dfs로 푼 답안
+    
+var longestZigZag = function(root) {
+    let answer = 0
+    zigZagPathCount(root, 0, false)
+    zigZagPathCount(root, 0, true)
+    return answer
+    
+    function zigZagPathCount(node, num, isLeft) {
+        if(!node) return
+
+        answer = Math.max(answer,num)
+        zigZagPathCount(node.left, isLeft === true ? num+1 : 1, false)
+        zigZagPathCount(node.right, isLeft === false ? num+1 : 1, true)
+
+    }
+};
+
+- 더 좋은 답안
+
+var longestZigZag = function(root) {
+    
+    let max = 0
+
+    function recursiveTraversal(node,parent){
+
+        if(!node){
+            return 0
+        }
+
+        const left = recursiveTraversal(node.left,"left")
+        const right = recursiveTraversal(node.right,"right")
+
+        max = Math.max(left,right,max)
+
+        if(parent==="left"){
+            return 1 + right
+        }
+
+        return 1 + left
+
+    }
+    
+    recursiveTraversal(root)
+    return max
+};
+
+
+ */
