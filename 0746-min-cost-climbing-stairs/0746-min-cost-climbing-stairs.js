4379813735a76a13415a96a8bbfe330602f4a8f4
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2023-12-19 11:44:44 +0900

    Time: 55 ms (64.09%), Space: 44.2 MB (25.79%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -2,15 +2,52 @@
  * @param {number[]} cost
  * @return {number}
  */
-const minCostClimbingStairs = function (cost) {
+var minCostClimbingStairs = function(cost) {
   const dp = [];
-
-  for (let i = cost.length - 1; i >= 0; i--) {
-    let oneStep = cost[i] + (dp[i + 1] || 0);
-    let twoStep = cost[i] + (dp[i + 2] || 0);
-
-    dp[i] = Math.min(oneStep, twoStep);
+  dp[0] = cost[0];
+  dp[1] = cost[1];
+  for (let i = 2; i < cost.length; i++) {
+    dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
   }
-
-  return Math.min(dp[0], dp[1]);
+  return Math.min(dp[cost.length-1], dp[cost.length-2]);
 };
+/**
+- 내 답안
+
+var minCostClimbingStairs = function(cost) {
+  let min = Infinity
+  takeStep(cost.length, 1, 0)
+  takeStep(cost.length, 2, 0)
+  return min
+  function takeStep(start, step, total) {
+      if(start === undefined || start < 0) {
+          return min = Math.min(min, total)
+      }
+      takeStep(start-step, 1, total + (start-step >= 0 ? cost[start-step] : 0))
+      takeStep(start-step, 2, total + (start-step >= 0 ? cost[start-step] : 0))
+  }
+}
+
+
+- 코드리뷰
+
+코드는 주어진 문제를 해결하기 위해 잘 작성되어 있습니다. 그러나 몇 가지 개선 사항이 있습니다:
+
+1. 함수의 설명이 부족합니다. 주석을 추가하여 함수의 목적과 입력, 출력에 대해 설명하는 것이 좋습니다.
+2. takeStep 함수의 재귀 호출을 통해 모든 가능한 경로를 탐색하는 것은 비효율적일 수 있습니다. 동적 계획법(Dynamic Programming)을 사용하여 중복 계산을 피하고 성능을 향상시킬 수 있습니다.
+
+다음은 개선된 코드입니다:
+var minCostClimbingStairs = function(cost) {
+  const dp = [];
+  dp[0] = cost[0];
+  dp[1] = cost[1];
+  for (let i = 2; i < cost.length; i++) {
+    dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+  }
+  return Math.min(dp[cost.length-1], dp[cost.length-2]);
+};
+/**
+
+
+
+ */
