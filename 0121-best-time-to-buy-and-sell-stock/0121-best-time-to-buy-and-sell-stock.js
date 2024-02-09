a520e6e3c2131050e9348881ef56aa93b8857cbe
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2024-02-09 16:45:46 +0900

    Time: 80 ms (33.05%), Space: 58.9 MB (26.99%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let left=0,right=1,max=0
+    
+    while(right<prices.length) {
+        if(prices[left]<prices[right]) {
+            max = Math.max(max, prices[right]-prices[left])
+        } else {
+            left=right
+        }
+        right++
+    }
+    
+    return max
+};
