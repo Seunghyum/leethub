cf28cc02cc8086acc2d25bdb2f9a73ffe5ccb062
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2024-02-11 14:42:25 +0900

    Time: 89 ms (13.33%), Space: 53.6 MB (5.88%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,17 +3,8 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    // if(n===1) return true
-    // else if(Math.abs(n)%2 !== 0) return false
-    // else {
-    //     return isPowerOfTwo(n/2)
-    // }
-    
-    if(n < 1) return false
     let i=0
-    // console.log("num : ", num)
     while(n >= Math.pow(2,i)) {
-        // console.log('Math.pow(2,i) : ', Math.pow(2,i) )
         if(Math.pow(2,i) === n) return true
         i++
     }
