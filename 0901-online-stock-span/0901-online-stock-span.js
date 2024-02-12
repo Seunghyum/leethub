006b6054f949064f596a3e7200b79e411c84cf53
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2024-02-12 09:04:48 +0900

    Time: 1543 ms (5.14%), Space: 61.5 MB (50.72%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.js
+++ 0901-online-stock-span/0901-online-stock-span.js
@@ -1,57 +1,12 @@
-
-// var StockSpanner = function() {
-//     let st = []
-    
-    
-// };
-
-
 class StockSpanner {
     st = [];
     next(price) {
         let i=this.st.length > 0 ? this.st.length-1 : 0
-        // console.log('====== price : ', price)
-        // console.log('this.st : ', this.st)  
         while(i >= 0 && this.st[i]<=price) {
-            // console.log('i : ', i)
             i--
         }
-
-        // console.log('final i : ', i)
-
-        // this.st = this.st.slice(i)
         const answer = this.st.length - i === 0 ? 1 : this.st.length - i
-        // console.log('price : ', price)
-        this.st.push(price)
-        // console.log('after this.st : ', this.st)    
+        this.st.push(price)   
         return answer
     }
 }
-
-// /** 
-//  * @param {number} price
-//  * @return {number}
-//  */
-// StockSpanner.prototype.next = function(price) {
-//     let st = []
-//     let i=st.length > 0 ? st.length-1 : 0
-//     console.log('====== price : ', price)
-//     while(i > 0 && st[i]>price) {
-//         console.log('i : ', i)
-//         i--
-//     }
-    
-//     console.log('i : ', i)
-    
-//     st = st.slice(i)
-//     console.log('st : ', st)    
-//     const answer = st.length - i + 1
-//     st.push(price)
-//     return answer
-// };
-
-// /** 
-//  * Your StockSpanner object will be instantiated and called as such:
-//  * var obj = new StockSpanner()
-//  * var param_1 = obj.next(price)
-//  */
