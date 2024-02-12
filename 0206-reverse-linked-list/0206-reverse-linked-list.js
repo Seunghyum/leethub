cbc95b134560ae162694268771d56663cdf855b9
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2024-02-13 06:05:35 +0900

    Time: 59 ms (52.94%), Space: 51.8 MB (20.01%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(head === null) return null
+    
+    let cur = head
+    const arr = []
+    arr.push(cur)
+    while(cur.next) {
+        cur = cur.next
+        arr.push(cur)
+    }
+    
+    for(let i=arr.length-1;i>=0;i--) {
+        arr[i].next = arr[i-1] || null
+    }
+    
+    return arr[arr.length-1]
+};
