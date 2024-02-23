1d1b2bc739eca88b3b4ce43ca2ec844a6b13bbaf
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2024-02-23 14:22:09 +0900

    Time: 67 ms (15.86%), Space: 52.6 MB (9.09%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -3,11 +3,27 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target, start=0,end=nums.length-1) {
-    const mid = Math.floor((end+start)/2)
-    if(nums[mid] === target) return mid
-    if(end <= start) return -1
+// var search = function(nums, target, start=0,end=nums.length-1) {
+//     const mid = Math.floor((end+start)/2)
+//     if(nums[mid] === target) return mid
+//     if(end <= start) return -1
 
-    if(nums[mid] > target) return search(nums, target, start,mid-1)
-    else if(nums[mid] < target) return search(nums, target, mid+1,end)
-};
+//     if(nums[mid] > target) return search(nums, target, start,mid-1)
+//     else if(nums[mid] < target) return search(nums, target, mid+1,end)
+// };
+
+const search = (nums, target) => {
+    let lo=0, hi=nums.length-1
+    
+    while(lo<hi) {
+        const mid = lo + Math.floor((hi-lo+1)/2)
+        
+        if(target < nums[mid]) {
+            hi=mid-1
+        } else {
+            lo = mid
+        }
+    }
+    
+    return target === nums[lo] ? lo : -1
+}
