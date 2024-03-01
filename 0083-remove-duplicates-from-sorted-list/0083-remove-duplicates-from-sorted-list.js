9adfc592c12f1b1d53ea39076f759a261f734a98
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2024-03-02 07:07:01 +0900

    Time: 68 ms (39.66%), Space: 53.3 MB (5.34%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -9,16 +9,31 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
 var deleteDuplicates = function(head) {
-    deleteNode(head)
-    return head
-    function deleteNode(head, next = head?.next) {
-        if(!head) return
-        if(next?.val === head.val) {
-           head.next = next.next
-           deleteDuplicates(head, next)
+    if(!head || !head?.next) return head
+    let cur = head
+    while(cur && cur?.next) {
+        
+        if(cur.val === cur.next.val) {
+            cur.next = cur.next.next
         } else {
-            deleteDuplicates(next, next?.next)
+            cur = cur.next
         }
     }
+    return head
 };
+
+// var deleteDuplicates = function(head) {
+//     deleteNode(head)
+//     return head
+//     function deleteNode(head, next = head?.next) {
+//         if(!head) return
+//         if(next?.val === head.val) {
+//            head.next = next.next
+//            deleteDuplicates(head, next)
+//         } else {
+//             deleteDuplicates(next, next?.next)
+//         }
+//     }
+// };
