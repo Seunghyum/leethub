1a4cc566a5bf3e36e99be74c319b7375421c28e2
Dave Moon (문승현) <moonseunghyun@zigbang.com>
2024-03-02 07:35:23 +0900

    Time: 630 ms (53.67%), Space: 89.8 MB (73.76%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -9,20 +9,37 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
 var deleteMiddle = function(head) {
-    if(!head) return head
-    const address = []
-    let cur = head, count = 0
-    while(cur) {
-        address.push(cur)
-        count++
-        cur = cur.next
+    if(!head) return null
+    
+    let prev = slow = new ListNode(0,head)
+    let fast = head
+    
+    while(fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
     }
     
-    count = Math.ceil(count/2) + (count%2 === 1 ? -1 : 0)
-    console.log('count : ', count)
-    if(count) address[count-1].next = address[count+1] || null
-    else return null
+    slow.next = slow.next?.next
     
-    return head
-};
+    return prev.next
+}
+
+// var deleteMiddle = function(head) {
+//     if(!head) return head
+//     const address = []
+//     let cur = head, count = 0
+//     while(cur) {
+//         address.push(cur)
+//         count++
+//         cur = cur.next
+//     }
+    
+//     count = Math.ceil(count/2) + (count%2 === 1 ? -1 : 0)
+//     console.log('count : ', count)
+//     if(count) address[count-1].next = address[count+1] || null
+//     else return null
+    
+//     return head
+// };
